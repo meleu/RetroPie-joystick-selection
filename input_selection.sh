c64d94fe5716206b9476eb345d5c44bac3c2fb5c
meleu <meleuzord@yahoo.com>
2016-05-22 17:22:18 +0000

    added joystick support

--- input_selection.sh
+++ input_selection.sh
@@ -21,38 +21,67 @@
 #        and let the user quit.
 #      - [robustness] verify if the "#include ...input-selection.cfg" line
 #        is before any input_playerN_joypad_index in the retroarch.cfg.
-#      - if jslist is not in the PATH, download it from github, compile it,
-#        and put it in $HOME/bin directory.
 #
-# meleu, 2016/05/21
+# meleu, 2016/05
 
 
-jsListFile="/tmp/jslist-$$"
-tempFile="${jsListFile}_d"
-retroarchcfg="/opt/retropie/configs/all/retroarch.cfg"
-inputcfg="/opt/retropie/configs/all/input-selection.cfg"
+rootdir="/opt/retropie"
+configdir="$rootdir/configs"
+
+js_list_file="/tmp/jslist-$$"
+temp_file="${js_list_file}_d"
+retroarchcfg="$configdir/all/retroarch.cfg"
+inputcfg="$configdir/all/input-selection.cfg"
+
+
+# borrowed code from runcommand.sh ############################################
+# The joy2key.py aren't documented, so I don't know how to use it... :(
+function start_joy2key() {
+    # if joy2key.py is installed run it with cursor keys for axis,
+    # and enter + tab for buttons 0 and 1
+    __joy2key_dev=$(ls -1 /dev/input/js* 2>/dev/null | head -n1)
+    if [[ -f "$rootdir/supplementary/runcommand/joy2key.py" && -n "$__joy2key_dev" ]] && ! pgrep -f joy2key.py >/dev/null; then
+        "$rootdir/supplementary/runcommand/joy2key.py" "$__joy2key_dev" 1b5b44 1b5b43 1b5b41 1b5b42 0a 09 &
+        __joy2key_pid=$!
+    fi
+}
+
+function stop_joy2key() {
+    if [[ -n "$__joy2key_pid" ]]; then
+        kill -INT "$__joy2key_pid"
+    fi
+}
+# end of the borrowed code from runcommand.sh #################################
+
+
+start_joy2key
 
 # checking if the "#include ..." line is in the retroarch.cfg
-grep -q '^#include "/opt/retropie/configs/all/input-selection.cfg"$' $retroarchcfg || {
-    dialog \
-      --title "Error" \
-      --yesno \
+grep -q \
+  "^#include \"$configdir/all/input-selection.cfg\"$"\
+  $retroarchcfg || {
+      dialog \
+        --title "Error" \
+        --yesno \
 "Your retroarch.cfg isn't properly configured to work with this method of \
 input selection. You need to put the following line on your \"$retroarchcfg\" \
 (preferably at the beginning)\
-\n\n#include \"/opt/retropie/configs/all/input-selection.cfg\"\n\n\
+\n\n#include \"$configdir/all/input-selection.cfg\"\n\n\
 Do you want me to put it at the beginning of the retroarch.cfg now?\
 \n(if you choose \"No\", I will stop now)" \
-      0 0 || exit 1;
+        0 0 || {
+            stop_joy2key
+            exit 1;
+        }
 
-    # Put the "#include ..." at the beginning line of retroarch.cfg
-    sed -i "1i\
+      # Putting the "#include ..." at the beginning line of retroarch.cfg
+      sed -i "1i\
 # $(date +%Y-%m-%d): The following line was added to allow input selection\n\
-#include \"/opt/retropie/configs/all/input-selection.cfg\"\n" $retroarchcfg
-}
+#include \"$configdir/all/input-selection.cfg\"\n" $retroarchcfg
+} # end of failed grep
 
 # if the input-selection.cfg doesn't exist, create it with default values
-[ -f "$inputcfg" ] || {
+[[ -f "$inputcfg" ]] || {
     cat << _EOF_ > $inputcfg
 # This file is used to choose what controller to use for each player.
 input_player1_joypad_index = "0"
@@ -62,51 +91,52 @@ input_player4_joypad_index = "3"
 _EOF_
 }
 
-
 # checking if jslist is on the PATH
 which jslist > /dev/null || {
-    dialog --title 'Fail!' --msgbox '"jslist" not found!' 5 40 
+    dialog --title "Fail!" --msgbox "\"jslist\" not found!" 5 40 
+    stop_joy2key
     exit 1
 }
 
-# the jslist returns a non-zero value if it fails or doesn't find any joystick
-jslist > $tempFile || {
+# the jslist returns a non-zero value if it doesn't find any joystick
+jslist > $temp_file || {
     dialog --title "Fail!" --msgbox "No joystick found. :(" 5 40 
-    rm -f $tempFile
+    rm -f $temp_file
+    stop_joy2key
     exit 1
 }
-
 
 # This obscure command searches for duplicated joystick names and puts
 # a sequential number at the end of the repeated ones
 # credit goes to fedorqui (http://stackoverflow.com/users/1983854/fedorqui)
 awk -F: 'FNR==NR {count[$2]++; next}
          count[$2]>1 {$0=$0 OFS "#"++times[$2]}
-         1' $tempFile $tempFile > $jsListFile
+         1' $temp_file $temp_file > $js_list_file
 
 # No need for this file anymore
-rm -f $tempFile
+rm -f $temp_file
 
-tempFile="/tmp/jstmp-$$"
+temp_file="/tmp/jstmp-$$"
 
 for i in $(seq 1 4); do
     # Obtaining the joystick list with the format
     # index "Joystick Name"
     # to use as dialog menu options
-    dialogOptions=$(sed 's/:\(.*\)/ "\1"/' $jsListFile)
+    dialogOptions=$(sed 's/:\(.*\)/ "\1"/' $js_list_file)
 
     echo "$dialogOptions" |
     xargs dialog \
         --title "Input selection" \
-        --menu "Which controller you want to use for \"Player $i\"?" \
-        0 0 0 2> $tempFile
+        --menu "Which controller you want to use for Player $i?" \
+        0 0 0 2> $temp_file
 
-    jsIndex=$(cat $tempFile)
+    js_index=$(cat $temp_file)
 
     # Here is the magic! Change the input_playerX_joypad_index in retroarch.cfg
-    sed "s/^input_player${i}_joypad_index.*/input_player${i}_joypad_index = \"$jsIndex\"/" $inputcfg > $tempFile
+    sed "s/^input_player${i}_joypad_index.*/input_player${i}_joypad_index = \"$js_index\"/" $inputcfg > $temp_file
 
-    mv $tempFile $inputcfg
+    mv $temp_file $inputcfg
 done
 
-rm -f $jsListFile 
+stop_joy2key
+rm -f $js_list_file 
--- install.sh
+++ install.sh
@@ -8,21 +8,24 @@
 # - certifies that $HOME/bin is in your PATH.
 
 echo "Checking if \"$HOME/bin\" exists, creating it if doesn't..."
-[ -d "$HOME/bin" ] || mkdir "$HOME/bin";
+[[ -d "$HOME/bin" ]] || mkdir "$HOME/bin"
 
 echo "Compiling \"jslist.c\" and putting it in \"$HOME/bin\"..."
 gcc jslist.c -o "$HOME/bin/jslist" $(sdl2-config --cflags --libs) || {
-    echo "Something wrong with compilation process. Aborting..."
+    echo -e "\nSomething wrong with the compilation process. Aborting..."
     exit 1
 }
 
 echo "Putting \"input_selection.sh\" in \"$HOME/bin\"..."
-cp input_selection.sh "$HOME/bin/input_selection.sh"
+cp input_selection.sh "$HOME/bin/input_selection.sh" || {
+    echo -e "\nUnable to put \"input_selection.sh\" in \"$HOME/bin\". Aborting."
+    exit 1
+}
 
 echo "Certifying that \"$HOME/bin\" is in your PATH..."
 echo "$PATH" | grep -q "$HOME/bin" || {
     export PATH="$HOME/bin:$PATH"
-    grep -q '^ *PATH="$HOME/bin:$PATH"' "$HOME/.profile" ||
-        echo PATH="$HOME/bin:$PATH" >> "$HOME/.profile";
+    grep -q '^ *PATH="$HOME/bin:$PATH"' "$HOME/.profile" \
+    || echo PATH="$HOME/bin:$PATH" >> "$HOME/.profile";
 
 }
